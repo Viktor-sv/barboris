1feababf2b626d69d59e3e59ddd2798b280b2b95
viktor <operator@com>
2016-04-15 21:05:45 +0300

    add allert

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -10,10 +10,11 @@ class Cocktail < ActiveRecord::Base
   validates :name, presence: true, allow_blank: false
 
   accepts_nested_attributes_for :ingredients,reject_if:  lambda {| attributes| attributes[:value].blank? && attributes[:product].blank? }
-
+  validate :validate_ingredients
 
   def value
     #ingredients.joins(:product).where('products.product_type = ?', 'drink').sum(:value)
+
     ingredients.inject(0) { |sum, ingredient| sum + (ingredient.product.product_type == 'drink' ? ingredient.value : 0 )
     }
   end
@@ -22,5 +23,15 @@ class Cocktail < ActiveRecord::Base
     #ingredients.all.inject(0){|price,ingredient| price + ingredient.price}
     ingredients.inject(0) { |price, ingredient| price + (ingredient.value / ingredient.product.min_value)*ingredient.price}.ceil
   end
+
+  def validate_ingredients
+     if ingredients.select { |sel| sel.product.product_type == 'drink' }.size < 2
+
+     #  errors.add(:value, "for cocktails '#{Cocktail.name}' must contain at list two drinkable product " )
+     end
+
+
+    end
+
 
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -4,9 +4,28 @@ class Ingredient < ActiveRecord::Base
 
   validates :value, numericality: {greater_than: 0}
   validates :product_id, presence:  true, allow_blank: false
+
+  validate :validate_value
   def price
 
     product.nil? ? 0: (value/product.min_value)*product.price
+
+  end
+
+
+
+
+  def validate_value
+    puts "validates"
+
+    if  product.present?
+
+      if product.min_value > value
+        errors.add(:value, "for product '#{product.name}' must be greater than ingredient min_value" )
+        puts "*******************"
+      end
+
+    end
 
   end
 end
