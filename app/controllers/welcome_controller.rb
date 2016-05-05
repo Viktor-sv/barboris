ce50bc4b38ed6a10126905581ae747eb6fe8bab6
viktor <viktor.stegachov@gmail.com>
2016-05-05 13:52:40 +0300

    try to add welcome search

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,25 @@
 class WelcomeController < ApplicationController
 
-
-
   def index
 
   end
 
+  def show
+    #@cocktails = Cocktail.all
+      redirect_to action:  'index'
 
+  end
+
+  def search_by_product (product)
+         #@cocktails = Cocktail.includes(:ingredients,:products).includes()
+    @cocktails = Cocktail.all.pick(product)
+
+    @cocktails.each do |cocktail|
+      puts "gehh"
+      putd cocktail.name.to_s
+
+    end
+
+    end
 
 end
