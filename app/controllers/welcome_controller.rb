da951d4de655b218cee56636cf3e271c11ff9e87
viktor <viktor.stegachov@gmail.com>
2016-05-14 21:49:16 +0300

    ned to add only show

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,12 +5,17 @@ class WelcomeController < ApplicationController
   end
 
   def show
-    @cocktails = Cocktail.includes(:ingredients,:products)
+    def show
+      @coctailes = Coctaile.joins(:products).where(ingredients: {product_id: params[:id]}).distinct
+      @product = Product.find_by id: params[:id]
+      
+    end
   end
 
   def search_by_product (product)
 
-    @cocktails = Cocktail.joins(:products).where(ingreients: {product_id: params[:id]}).district
+   # @cocktails = Cocktail.joins(:products).where(ingreients: {product_id: params[:id]}).district
+   #  redirect_to 'show'
 
     end
 
